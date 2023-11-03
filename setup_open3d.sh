d8dea8829432594baf12326ae2cd0a74b3dbe227
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2023-11-03 11:05:11 +0800

    Delete setup_open3d.sh

--- setup_open3d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update package list
-sudo apt-get update
-
-# Install required packages
-sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev libc++-13-dev libc++abi-13-dev libx11-dev libxrandr-dev libxi-dev libxxf86vm-dev libxinerama-dev libxcursor-dev libxinerama-dev
