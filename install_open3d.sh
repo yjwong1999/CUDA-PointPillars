fdefe97c02a8c7747fb84bddabd2f2f5b838eca4
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2023-11-03 11:04:53 +0800

    Update install_open3d.sh

--- install_open3d.sh
+++ install_open3d.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Update package list
+sudo apt-get update
+
+# Install required packages
+sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev libc++-13-dev libc++abi-13-dev libx11-dev libxrandr-dev libxi-dev libxxf86vm-dev libxinerama-dev libxcursor-dev libxinerama-dev
+
+
 # Clone the Open3D repository
 git clone --recursive https://github.com/intel-isl/Open3D.git
 
